6c5169e58b0b40eb12925f131cfa8ca1c50cc373
Dory-kr <rey.mitani@gmail.com>
2026-06-25 11:03:27 +0300

    Complete homework 11

---
+++ src/js/main.js
@@ -0,0 +1,56 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './render-functions';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSearchSubmit);
+
+async function onSearchSubmit(event) {
+  event.preventDefault();
+
+  const query = event.currentTarget.elements['search-text'].value.trim();
+
+  if (query === '') {
+    iziToast.warning({
+      message: 'Please enter a search query',
+      position: 'topRight',
+    });
+
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query);
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+
+      return;
+    }
+
+    createGallery(data.hits);
+  } catch (error) {
+    iziToast.error({
+      message: 'Something went wrong. Please try again later.',
+      position: 'topRight',
+    });
+  } finally {
+    hideLoader();
+    event.currentTarget.reset();
+  }
+}
